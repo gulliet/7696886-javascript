0ae6553430f1d975b9851eb4676774b45bda87d6
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-11-23 09:24:12 +0100

    Cleaned up project

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,20 @@
 /*********************************************************************************
- * 
- * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
- * 
+ *
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu.
+ *
  *********************************************************************************/
 
-lancerJeu()
+lancerJeu();
+
+// let btnValiderMot = document.getElementById("btnValiderMot");
+// console.log(btnValiderMot);
+// btnValiderMot.addEventListener("click", () => {
+//     console.log("Clicker sur le boutton vous avez !");
+// });
+
+// document.addEventListener("keydown", (event) => {
+//     console.log(event.target, event.key, event.clientX, event.clientY);
+// });
 
 // J'ai mis ce code en commentaire, nous pourrons le récupérer lorsque nous en auront besoin :
 
