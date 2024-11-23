dbcc1841c70b192550ca72863b5bfc43344500c1
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-11-23 18:35:42 +0100

    Fin de l'exercice.

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,20 @@
  */
 function afficherResultat(score, nbMotsProposes) {
     console.log("Votre score est de " + score + " sur " + nbMotsProposes);
+    let zoneScore = document.querySelector("zoneScore span");
+    zoneScore.textContent = `${score} / ${nbMotsProposes}`;
+}
+
+function afficherProposition(mot) {
+    let zoneProposition = document.querySelector(".zoneProposition");
+    console.log(zoneProposition);
+    zoneProposition.textContent = mot;
+}
+
+function resetInputEcriture() {
+    let inputEcriture = document.getElementById("inputEcriture");
+    console.log(inputEcriture);
+    inputEcriture.value = "";
 }
 
 /**
@@ -21,6 +35,35 @@ function lancerJeu() {
     // Initialisations
     let score = 0;
     let nbMotsProposes = 0;
+
+    let i = 0;
+    console.log(listeMots[i]);
+    afficherProposition(listeMots[i]);
+    nbMotsProposes++;
+
+    let btnValiderMot = document.getElementById("btnValiderMot");
+    btnValiderMot.addEventListener("click", () => {
+        if (listeMots[i] == inputEcriture.textContent) {
+            score++;
+        }
+        afficherResultat(score, nbMotsProposes);
+
+        i++;
+        console.log(i, "Boutton clicker !");
+        if (i < listeMots.length) {
+            afficherProposition(listeMots[i]);
+            nbMotsProposes++;
+        } else {
+            afficherProposition("Le jeu est fini");
+            btnValiderMot.disabled = true;
+        }
+        resetInputEcriture();
+    });
+
+    let inputEcriture = document.getElementById("inputEcriture");
+    inputEcriture.addEventListener("keydown", (event) => {
+        console.log(inputEcriture.value);
+    });
 
     afficherResultat(score, nbMotsProposes);
 }
